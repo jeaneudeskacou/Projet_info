2e90849f30b6b1162eb3c9b0850e9e1b06883c0d
jeaeudeskacou <kacoukbje@gmail.com>
2020-04-21 21:43:15 +0200

    Ouhm lot of thing have change

--- daemon.py
+++ daemon.py
@@ -1,3 +1,15 @@
+"""ce module lit la table de routage et ecrit les données dans des fichiers
+comme ceci.
+{1587496787.5253153:
+ {'ligne 1': {'DESTINATION': '0.0.0.0', 'PASSERELLE': '192.168.117.2', 'GENMASK': '0.0.0.0', 'INDIC': 'UG', 'METRIC': '100', 'REF': '0', 'USE': '0', 'IFACE': 'ens33'},
+  'ligne 2': {'DESTINATION': '169.254.0.0', 'PASSERELLE': '0.0.0.0', 'GENMASK': '255.255.0.0', 'INDIC': 'U', 'METRIC': '1000', 'REF': '0', 'USE': '0', 'IFACE': 'ens33'},
+  'ligne 3': {'DESTINATION': '192.168.117.0', 'PASSERELLE': '0.0.0.0', 'GENMASK': '255.255.255.0', 'INDIC': 'U', 'METRIC': '100', 'REF': '0', 'USE': '0', 'IFACE': 'ens33'}
+  }
+}
+dans sa presente version, le module produit 2 fichiers, un fichier binaire contenant la donnée sous forme d'objet (donc facilement exploitable par l'ordinateur mais
+illisible pour l'homme)
+un fichier .txt destiné à etre lu par l'homme.
+"""
 import os
 import time
 import pickle
@@ -8,33 +20,30 @@ OUTPUT_DIR_NAME = "Experiment_outputs"
 OUTPUT_DIR_PATH = os.path.join(os.getenv("HOME"), OUTPUT_DIR_NAME)
 META_FILE = os.path.join(OUTPUT_DIR_PATH, "meta.txt")
 TABLE = os.path.join(OUTPUT_DIR_PATH, "table.tmp")
-#BINARY = os.path.join(OUTPUT_DIR_PATH, "binary.tmp")
-
-
-DESTINATION = 0
-PASSERELLE = 1
-GENMASK = 2
-INDIC = 3
-METRIC = 4
-REF = 5
-USE = 6
-IFACE = 7
-
 
 def initialization():
-    """Fonction pour creer le dossier ou seront stockés les sorties
-    Ainsi qu'un fichier servant a retrouver le nombre d'experiences realisée
-    Leur donner les droits necessaire etc"""
-    if os.path.exists(OUTPUT_DIR_PATH):
-        with open(META_FILE, "rb") as meta_file:
-            number = struct.unpack("i", meta_file.read(4))
-            for i in number:
-                result = i+1
-                break
-        with open(META_FILE, "wb") as meta_file:
-            meta_file.write(struct.pack("i", result))
-        return result
-
+    """Fonction pour creer le dossier où seront stockées les sorties
+    ainsi qu'un fichier servant a retrouver le nombre d'experiences réalisée
+    Leur donner les droits necessaire etc. Si le dossier existe deja, on lit juste le
+    fichier meta.txt et on renvoi la valeur lu"""
+    if os.path.exists(OUTPUT_DIR_PATH) :
+        if os.path.exists(META_FILE):
+            with open(META_FILE, "rb") as meta_file:
+                number = struct.unpack("i", meta_file.read(4))
+                for i in number:
+                    result = i+1
+                    break
+            with open(META_FILE, "wb") as meta_file:
+                meta_file.write(struct.pack("i", result))
+            return result
+        else:
+            create_meta = "touch " + META_FILE
+            os.system(create_meta)
+            chmod_META = "chmod a+w " + META_FILE
+            os.system(chmod_META)
+            with open(META_FILE, "wb") as meta_file:
+                meta_file.write(struct.pack("i", 0))
+            return 1
     else:
         os.mkdir(OUTPUT_DIR_PATH)
         create_meta = "touch "+ META_FILE
@@ -48,8 +57,8 @@ def initialization():
 
 def new_Experiment():
     """Fonction pour creer les fichiers necessaires pour une nouvelle experience
-    notamment le fichier de sorties. Elle reinitialise aussi les fichiers intermediaires
-    comme le BINARY et le TABLE"""
+    notamment le fichier de sorties. Elle reinitialise aussi le fichier intermediaire
+    comme le TABLE"""
 
     xp_number = initialization()
     OUTPUT = os.path.join(OUTPUT_DIR_PATH, "experiment_" + str(xp_number)+".txt" )
@@ -112,28 +121,51 @@ def getTabledata(src=TABLE):
         text = text.split("\n")
     for line in range(len(text) - 3):
         lg = text[line + 2].split(' ')
-        elmt = 0
-        while elmt < len(lg) - 1:
-            if lg[elmt] in " ":
-                lg.pop(elmt)
-            else:
-                lg[elmt].strip()
-                elmt += 1
         cle = "ligne " + str(line+1)
-        data[cle] = parseTableData(lg)
+        data[cle] = parseTableData(tablerefactor(lg))
     return data
 
 
 def getTime():
-    if os.path.exists("/dev/ttyUSB0"):
-        ser = serial.Serial("/dev/ttyUSB0",4800, timout =2)
+    """if os.path.exists("/dev/ttyUSB0"):
+        ser = serial.Serial("/dev/ttyUSB0", 2400, timeout=3)
         while True:
-            line = ser.readline()
-            line = line.split(',')
-            if line[0] == "§GPRMC":
-                return line[1]
-    else:
-        return time.time()
+            line = str(ser.readline())
+            print("time from gps")
+            splitline = line.split(',')
+
+            if splitline[0] == "b'$GPRMC":
+                result = splitline[1]
+                break"""
+
+    result = time.time()
+    return result
+
+"""def getTime():
+    result = ""
+    if os.path.exists("/dev/ttyUSB0"):
+        print("getTime function")
+        cmd = "cat < /dev/ttyUSB0 > time.tmp"
+        os.system(cmd)
+        with open("time.tmp", "r") as file:
+            while True:
+                print("iteration")
+                line = file.readline()
+                splitline = line.split(',')
+                elmt = 0
+                while elmt < len(splitline) - 1:
+                    if splitline[elmt] in " ":
+                        splitline.pop(elmt)
+                    else:
+                        splitline[elmt].strip()
+                        elmt += 1
+                print(splitline)
+                if splitline[0] == "$GPRMC":
+                    print("find GPRMC")
+                    result = splitline[1]
+                    print(result)
+                    break
+    return result"""
 
 
 def getLastInput(dst):
@@ -192,18 +224,36 @@ def readData(dest, scr):
             with open(dest, "w") as dest:
                 dest.write(str(data))
 
-def getGPSData():
-    if os.path.exists("/dev/ttyUSB0"):
-        ser = serial.Serial("/dev/ttyUSB0",4800, timout =2)
-        while True:
-            line = ser.readline()
-            line = line.split(',')
-            gpsdata = {}
-            if line[0]=="§GPRMC":
-                gpsdata
-    else:
-        print("NO GPS MODULE CONNECTED")
 
+def tablerefactor(table):
+    elmt = 0
+    while elmt < len(table) - 1:
+        if table[elmt] in " ":
+            table.pop(elmt)
+        else:
+            table[elmt].strip()
+            elmt += 1
+    return table
+
+"""def getGPSData():
+    if os.path.exists("/dev/ttyUSB0"):
+        ser = serial.Serial("/dev/ttyUSB0", 2400, timeout=3)
+        while True:
+            line = str(ser.readline())
+            splitline = line.split(',')
+            if splitline[0] == "b'$GPRMC":
+                result = splitline
+                break
+        elmt = 0
+        while elmt < len(splitline) - 1:
+            if splitline[elmt] in " ":
+                splitline.pop(elmt)
+            else:
+                splitline[elmt].strip()
+                elmt += 1
+        return result
+    else:
+        print("NO GPS MODULE CONNECTED")"""
 
 
 def main():
@@ -211,9 +261,11 @@ def main():
     OUTPUT = xp[0]
     BINARY = xp[1]
     while (True):
-        print("=============================== Nouvelle Interation ===================================")
+        print("============================= Nouvelle Interation =================================")
         getTable()
         setInput(BINARY)
         readData(OUTPUT, BINARY)
         time.sleep(3)
     end_Experiment()
+
+main()
